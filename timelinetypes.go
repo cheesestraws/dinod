2a9f39ab9afd4dc00271ebbf9ac6a6dfe33e9cff
Rob Mitchelmore <rmitchelmore@twitch.tv>
2016-02-19 14:46:56 +0000

    updates

--- timelinetypes.go
+++ timelinetypes.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Timeline struct {
 	FriendlyName string           `json:"friendlyName"`
 	DinoName     string           `json:"dinoName"`
@@ -7,4 +9,16 @@ type Timeline struct {
 	Length       int              `json:"length"`
 	TimePerStep  float64          `json:"timePerStep"`
 	Timeline     map[string][]int `json:"timeline"`
+}
+
+func (t Timeline) Validate(dinos Dinos) error {
+	if t.DinoName == "" {
+		return fmt.Errorf("no dinoName provided")
+	}
+
+	if dinos.FindDino(t.DinoName) == nil {
+		return fmt.Errorf("dinoName %v supplied but this dino does not exist", t.DinoName)
+	}
+
+	return nil
 }
