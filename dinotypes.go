e986241b6097b320082431bb078dfa7bb7e27ecd
Rob Mitchelmore <rmitchelmore@twitch.tv>
2016-02-21 16:55:15 +0000

    Golint tidies for dinotypes.go

--- dinotypes.go
+++ dinotypes.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 )
 
+/*
+A Dino struct contains the definition of a dinosaur (or, I guess, some other robot or
+device.  It does not contain the *runtime* state necessary to run the device, but only
+the static data that can exist between runs.  The 'backend' field is additional to the
+fields defined in the Dinosaur API.  This field defines the protocol that the dinod
+instance should use to talk to the device.
+*/
 type Dino struct {
 	Name         string         `json:"name"`
 	FriendlyName string         `json:"friendlyName"`
@@ -14,6 +21,10 @@ type Dino struct {
 	Backend      string         `json:"backend"`
 }
 
+/*
+Valid checks whether a Dino is valid.  If it is valid, it returns nil: if it is not,
+it returns an error describing the problem that can be passed back to the user.
+*/
 func (d Dino) Valid() error {
 	if d.Name == "" {
 		return errors.New("dinosaur has no name")
@@ -36,6 +47,10 @@ func (d Dino) Valid() error {
 	return nil
 }
 
+/*
+FindSensor returns the sensor description that corresponds to a given name for this Dino,
+or nil if none is found.
+*/
 func (d Dino) FindSensor(name string) *DinoSensor {
 	for _, sensor := range d.Sensors {
 		if sensor.Name == name {
@@ -46,6 +61,10 @@ func (d Dino) FindSensor(name string) *DinoSensor {
 	return nil
 }
 
+/*
+FindActuator returns the actuator description that corresponds to a given name for this
+Dino, or nil if none is found.
+*/
 func (d Dino) FindActuator(name string) *DinoActuator {
 	for _, actuator := range d.Actuators {
 		if actuator.Name == name {
@@ -56,13 +75,16 @@ func (d Dino) FindActuator(name string) *DinoActuator {
 	return nil
 }
 
-type DinoState struct {
-	Dino
-	gpio GPIO
-}
-
+/*
+The Dinos type contains a list of Dino definitions.  It exists so that it can have method
+definitions upon it.
+*/
 type Dinos []Dino
 
+/*
+FindDino returns the Dino definition with the given name from a list of Dino definitions.
+If no such dino exists, it returns nil.
+*/
 func (d Dinos) FindDino(name string) *Dino {
 	for _, dino := range d {
 		if dino.Name == name {
@@ -72,8 +94,24 @@ func (d Dinos) FindDino(name string) *Dino {
 	return nil
 }
 
+/*
+A DinoState struct contains a Dino and any state necessary to make it actually run.
+*/
+type DinoState struct {
+	Dino
+	gpio GPIO
+}
+
+/*
+The DinoStates type contains a list of Dino runtime states.  It exists so that it can
+have method definitions upon it.
+*/
 type DinoStates []DinoState
 
+/*
+FindDinoState returns the Dino state with the given name from a list of Dino states.  If
+no such dino exists, it returns nil.
+*/
 func (d DinoStates) FindDinoState(name string) *DinoState {
 	for _, dino := range d {
 		if dino.Dino.Name == name {
@@ -83,6 +121,10 @@ func (d DinoStates) FindDinoState(name string) *DinoState {
 	return nil
 }
 
+/*
+The DinoSensor type encodes the definition of a sensor.  Dinod only supports very simple
+sensors at this stage, and the 'type' field is basically ignored.
+*/
 type DinoSensor struct {
 	Name         string `json:"name"`
 	FriendlyName string `json:"friendlyName"`
@@ -90,18 +132,26 @@ type DinoSensor struct {
 	Pin          int    `json:"pin"`
 }
 
+/*
+Valid checks whether a sensor definition is valid.  It returns a human-readable error, or
+nil if the definition is valid.
+*/
 func (d DinoSensor) Valid() error {
 	if d.Name == "" {
 		return errors.New("sensor has no name")
 	}
 
 	if d.Type != "pulse" {
-		return errors.New(fmt.Sprintf("sensor %v has an invalid type", d.Name))
+		return fmt.Errorf("sensor %v has an invalid type", d.Name)
 	}
 
 	return nil
 }
 
+/*
+The DinoActuator type encodes the definition of a actuator.  Dinod only supports very simple
+actuators at this stage, and the 'type' field is basically ignored.
+*/
 type DinoActuator struct {
 	Name         string `json:"name"`
 	FriendlyName string `json:"friendlyName"`
@@ -109,13 +159,17 @@ type DinoActuator struct {
 	Pin          int    `json:"pin"`
 }
 
+/*
+Valid checks whether an actuator definition is valid.  It returns a human-readable error,
+or nil if the definition is valid.
+*/
 func (d DinoActuator) Valid() error {
 	if d.Name == "" {
 		return errors.New("actuator has no name")
 	}
 
 	if d.Type != "onoff" {
-		return errors.New(fmt.Sprintf("actuator %v has an invalid type", d.Name))
+		return fmt.Errorf("actuator %v has an invalid type", d.Name)
 	}
 
 	return nil
